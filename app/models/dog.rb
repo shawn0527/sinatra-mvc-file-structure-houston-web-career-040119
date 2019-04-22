86a2ec45636fd1644583a2754d7935c7671e10c5
Shawn Ren <shawnren527@gmail.com>
2019-04-22 20:08:17 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,7 +4,7 @@ class Dog
 
   @@all =
 
-  def initialize(name: name, breed: breed, age: age)
+  def initialize(name: name=nil, breed: breed=nil, age: age=nil)
     @name = name
     @breed = breed
     @age = age
